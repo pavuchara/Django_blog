ade617ab286318ce194fa27fbf2972f0d04f54ce
Marsel Narbekov <lllmarselll@gmail.com>
2024-03-22 12:02:58 +0300

    New app for tags New templates Posts have tags, and we can recommend same posts

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .forms import EmailPostForm
+from django.core.mail import send_mail
+from django.conf import settings
+from django.views.decorators.http import require_POST
+from django.db.models import Count
+from taggit.models import Tag
+
+from .forms import EmailPostForm, CommentForm
+from .models import Post
 
 
 def post_detail(request, year, month, day, post):
@@ -11,14 +17,33 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    # Список активных комментариев к посту.
+    comments = post.comments.filter(active=True)
+    # Форма для комментариев к посту.
+    form = CommentForm()
+
+    # Список схожих постов.
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    simular_posts = Post.published.filter(tags__id__in=post_tags_ids) \
+        .exclude(id=post.id)
+    simular_posts = simular_posts.annotate(same_tags=Count('tags')) \
+        .order_by('-same_tags', '-publish')[:4]
 
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'form': form,
+                   'comments': comments,
+                   'simular_posts': simular_posts})
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     posts_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        posts_list = posts_list.filter(tags__in=[tag])
+    # Постраничная разбивка с 3 постами на страницу
     paginator = Paginator(posts_list, 3)
     page_number = request.GET.get('page', 1)
     try:
@@ -29,19 +54,45 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'posts': posts,
+                   'tag': tag})
 
 
 def post_share(request, post_id):
     post = get_object_or_404(Post,
                              id=post_id,
                              status=Post.Status.PUBLISHED)
+    sent = False
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            # ... отправить письмо
-        else:
-            form = EmailPostForm()
-        return render(request, 'blog/posts/share.html', {'post': post,
-                                                         'form': form})
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f'{cd['name']} recommends you read{post.title}'
+            message = (f'Read {post.title} at {post_url}\n\n'
+                       f'{cd['name']}\"s ({cd['email']})'
+                       f'comments: {cd['comments']}')
+            send_mail(subject, message, settings.EMAIL_HOST_USER, [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             id=post_id)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html', {'post': post,
+                                                      'form': form,
+                                                      'comment': comment})
